bcf3614b91ab2963f94a05e8b255c49b09ae7d25
do1thingwell <do1thingwell@Macs-MBP.net.fpt>
2020-04-03 14:16:25 +0700

    cleanup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,18 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
+// // A $( document ).ready() block.
+// $( document ).ready(function() {
 
-	// DropCap.js
-	var dropcaps = document.querySelectorAll(".dropcap");
-	window.Dropcap.layout(dropcaps, 2);
+// 	// DropCap.js
+// 	var dropcaps = document.querySelectorAll(".dropcap");
+// 	window.Dropcap.layout(dropcaps, 2);
 
-	// Responsive-Nav
-	var nav = responsiveNav(".nav-collapse");
+// 	// Responsive-Nav
+// 	var nav = responsiveNav(".nav-collapse");
 
-	// Round Reading Time
-    $(".time").text(function (index, value) {
-      return Math.round(parseFloat(value));
-    });
+// 	// Round Reading Time
+//     $(".time").text(function (index, value) {
+//       return Math.round(parseFloat(value));
+//     });
 
-});
+// });
 
 
